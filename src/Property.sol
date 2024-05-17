e1632c5f1f1aabdb368b539987a12d9f4bdcc69d
RahulRST <srahuljayaraj@gmail.com>
2024-05-17 14:58:19 +0530

    openzeppelin trial

--- src/Property.sol
+++ src/Property.sol
@@ -1,63 +1,66 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.0;
 
-contract Property {
-    // Define a struct to represent a property
-    struct PropertyStruct {
-        string id;
-        string name;
-        string location;
-        uint256 price;
-        string description;
-        address owner;
-        bool leased;
-        address leasee;
-    }
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
-  // Define a mapping to store properties with their unique IDs as keys
-  mapping(string => PropertyStruct) public properties;
+contract Property is Ownable {
+  // Define a struct to represent a property
+  struct PropertyStruct {
+    uint256 id; // Use a counter for unique IDs
+    string name;
+    string location;
+    uint256 price;
+    string description;
+    address owner;
+    bool leased;
+    address leasee;
+  }
+
+  // Use a counter to generate unique IDs for properties
+  using Counters for Counters.Counter;
+  Counters.Counter private _propertyIds;
+
+  // Define a mapping to store properties with their IDs as keys
+  mapping(uint256 => PropertyStruct) public properties;
 
   // Function to create a new property
   function createProperty(
-    string memory _id,
     string memory _name,
     string memory _location,
     uint256 _price,
     string memory _description
-  ) public {
-    require(bytes(properties[_id].id).length == 0, "Property with this ID already exists");
-    properties[_id] = PropertyStruct(_id, _name, _location, _price, _description, msg.sender, false, address(0));
+  ) public onlyOwner {
+    uint256 newId = _propertyIds.current();
+    _propertyIds.increment();
+
+    properties[newId] = PropertyStruct(
+      newId,
+      _name,
+      _location,
+      _price,
+      _description,
+      msg.sender,
+      false,
+      address(0)
+    );
   }
 
   // Function to get details of a property by ID
-  function getProperty(string memory _id) public view returns (PropertyStruct memory) {
+  function getProperty(uint256 _id) public view returns (PropertyStruct memory) {
     return properties[_id];
   }
 
-  // Function to return an array of all properties
+  // Function to return an array of all properties (using a loop with counters)
   function getAllProperties() public view returns (PropertyStruct[] memory) {
-    // Initialize an empty array to store all properties
-    PropertyStruct[] memory allProperties = new PropertyStruct[](0);
+    uint256 totalProperties = _propertyIds.current();
+    PropertyStruct[] memory allProperties = new PropertyStruct[](totalProperties);
 
-    // Loop through the mapping and add each property to the array
-    for (string memory key in properties) {
-      allProperties = push(allProperties, properties[key]);
+    // Loop through all property IDs and fetch details
+    for (uint256 i = 0; i < totalProperties; i++) {
+      allProperties[i] = properties[i + 1]; // IDs start from 1
     }
-
 
     return allProperties;
-  }
-
-  // Helper function to add elements to a dynamic array (not built-in in Solidity)
-  function push(PropertyStruct[] memory array, PropertyStruct memory element) internal pure returns (PropertyStruct[] memory) {
-    uint newLength = array.length + 1;
-    PropertyStruct[] memory newArray = new PropertyStruct[](newLength);
-
-    for (uint i = 0; i < array.length; i++) {
-      newArray[i] = array[i];
-    }
-
-    newArray[newLength - 1] = element;
-    return newArray;
   }
 }
